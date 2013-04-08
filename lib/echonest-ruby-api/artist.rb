27023dc13ec2fb70750458c3e151fc945079309f
David Attilio Bellotti <dbellotti@gmail.com>
2013-04-08 16:28:17 -0700

    formats name @formatted_name to be compatable with uri and uses in place of @name variable so that @name is actual readable name

--- lib/echonest-ruby-api/artist.rb
+++ lib/echonest-ruby-api/artist.rb
@@ -13,12 +13,13 @@ module Echonest
 
     def initialize(api_key, name = nil, foreign_ids = nil)
       @name = name
+      @query_name = name.gsub(' ', '+')
       @api_key = api_key
       @foreign_ids = ForeignId.parse_array(foreign_ids) if foreign_ids
     end
 
     def biographies(options = { results: 1 })
-      response = get_response(results: options[:results], name: @name)
+      response = get_response(results: options[:results], name: @query_name)
 
       response[:biographies].collect do |b|
         Biography.new(text: b[:text], site: b[:site], url: b[:url])
@@ -26,7 +27,7 @@ module Echonest
     end
 
     def blogs(options = { results: 1 })
-      response = get_response(results: options[:results], name: @name)
+      response = get_response(results: options[:results], name: @query_name)
 
       response[:blogs].collect do |b|
         Blog.new(name: b[:name], site: b[:site], url: b[:url])
@@ -34,17 +35,17 @@ module Echonest
     end
 
     def familiarity
-      response = get_response(name: @name)
+      response = get_response(name: @query_name)
       response[entity_name.to_sym][__method__.to_sym]
     end
 
     def hotttnesss
-      response = get_response(name: @name)
+      response = get_response(name: @query_name)
       response[entity_name.to_sym][__method__.to_sym]
     end
 
     def images
-      response = get_response(name: @name)
+      response = get_response(name: @query_name)
       images = []
       response[:images].each do |i|
         images << i[:url]
@@ -57,7 +58,7 @@ module Echonest
     end
 
     def search(options = {})
-      options = {name: @name}.merge(options)
+      options = {name: @query_name}.merge(options)
       artists = []
       get_response(options)[:artists].each do |a|
         artists << Artist.new(@api_key, a[:name], a[:foreign_ids])
@@ -66,7 +67,7 @@ module Echonest
     end
 
     def similar(options = {})
-      options = {name: @name}.merge(options)
+      options = {name: @query_name}.merge(options)
       response = get_response(options)
       artists = []
       response[:artists].each do |a|
@@ -77,7 +78,7 @@ module Echonest
 
     def songs
       songs = []
-      get_response(name: @name)[:songs].each do |s|
+      get_response(name: @query_name)[:songs].each do |s|
         songs << { s[:id] => s[:title] }
       end
       songs
